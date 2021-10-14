2ec003d8a9c502b83b215a44c15a552e1bef5264
John Livingston <git@john-livingston.fr>
2021-10-14 12:32:13 +0200

    Fix.

--- htdocs/lib/presets/ansible.php
+++ htdocs/lib/presets/ansible.php
@@ -82,11 +82,11 @@ abstract class CocotsAnsiblePresets extends CocotsPresets {
 
     $spip_config = array();
     $write_spip_config = false;
-    if (defined(COCOTS_PRESETS_ANSIBLE_SPIP_CONFIG) && is_array(COCOTS_PRESETS_ANSIBLE_SPIP_CONFIG)) {
+    if (defined('COCOTS_PRESETS_ANSIBLE_SPIP_CONFIG') && is_array(COCOTS_PRESETS_ANSIBLE_SPIP_CONFIG)) {
       $write_spip_config = true;
       $spip_config = array_merge($spip_config, COCOTS_PRESETS_ANSIBLE_SPIP_CONFIG);
     }
-    if (defined(COCOTS_PRESETS_ANSIBLE_SPIP_USE_COCOTS_SMTP) && COCOTS_PRESETS_ANSIBLE_SPIP_USE_COCOTS_SMTP) {
+    if (defined('COCOTS_PRESETS_ANSIBLE_SPIP_USE_COCOTS_SMTP') && COCOTS_PRESETS_ANSIBLE_SPIP_USE_COCOTS_SMTP) {
       $write_spip_config = true;
       $facteur_config = array(
         'mailer' => 'smtp',
