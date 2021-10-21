ff8b8d165bd212e9ee851b5b20fe817050093bb4
John Livingston <git@john-livingston.fr>
2021-10-21 17:06:24 +0200

    CLI: Password asked by prompt.

--- cli/cocots.php
+++ cli/cocots.php
@@ -24,7 +24,7 @@ function print_usage() {
   echo("  moderators list verbose;              list moderators with additionnal informations.\n");
   echo("  moderators create 'mail@example.com'; Creates a moderator, and send invitation mail.\n");
   echo("  moderators invit_link 1;              Print the invitation link for this moderator.\n");
-  echo("  moderators activate 1 'password';     Equivalent to using the invitation link to setup a password. Use the moderator id as key.\n");
+  echo("  moderators activate 1;                Equivalent to using the invitation link to setup a password. Use the moderator id as key. Password will be asked with a prompt.\n");
   echo("  moderators revoke 1;                  Revokes a moderator by his id.\n");
   echo("  moderators delete 1;                  Delete a moderator by his id. Please prefer revocation, and use only deletion before re-creating.\n");
   echo("  test mail;                            Send a test mail to admins (not moderators).");
@@ -82,14 +82,16 @@ switch ($scope) {
           echo "Missing id.\n";
           exit(1);
         }
-        if (!$argv[4]) {
-          echo "Missing password.\n";
-          exit(1);
-        }
         $id = $argv[3];
-        $password = $argv[4];
         if (!filter_var($id, FILTER_VALIDATE_INT)) {
           echo "Invalid id.\n";
+          exit(1);
+        }
+
+        echo "New password?\n";
+        $password = trim(fgets(STDIN));
+        if (strlen($password) < 8) {
+          echo "Password too short\n";
           exit(1);
         }
 
